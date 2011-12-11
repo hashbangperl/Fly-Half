a54ccfdb50d34212137a37c85925fa03b54ea523
Aaron Trevena <aaron.trevena@gmail.com>
2011-12-11 06:29:29 +0000

    most E-R in place, tested against mysql

--- sql/create.sql
+++ sql/create.sql
@@ -1,5 +1,13 @@
 use flyhalf;
 
+create table if not exists estimate_unit (
+   id                              int not null auto_increment,
+   name				   varchar(20) not null,
+   abbreviation			   varchar(4),
+   primary key ( id )
+)  ENGINE=InnoDB;
+
+
 create table if not exists team (
         id                              int                             not null auto_increment,
         name	varchar(100),
@@ -7,14 +15,9 @@ create table if not exists team (
         unique  key name ( name ),
 	current_sprint int references sprint(id),
 	daily_developer_capacity int default 5,
-	daily_developer_capacity_units int references estimate_unit(id),
+	daily_developer_capacity_units int,
+	foreign key (daily_developer_capacity_units) references estimate_unit(id),
         primary key ( id )
-) ENGINE=InnoDB;
-
-create table if not exists team_sprints (
-       team_id int not null references team(id),
-       sprint_id int not null references sprint(id),
-       primary key (team_id, sprint_id)
 ) ENGINE=InnoDB;
 
 create table if not exists users (
@@ -22,154 +25,179 @@ create table if not exists users (
         username                varchar(50)     not null,
         password                varchar(74)     not null,
         email                   varchar(200)    not null,
-
-        firstname               varchar(50)             ,
-        surname                 varchar(50)             ,
-
-        profile_pic             varchar(100)    ,
-        profile_description     text                    ,
-
-        location                varchar(100)    ,
-
-        active                  int                             not null default 1,
-	team			int		references team(id),
-
+        firstname               varchar(50),
+        surname                 varchar(50),
+        profile_pic             varchar(100),
+        profile_description     text,
+        location                varchar(100),
+        active                  int not null default 1,
+	team			int,
+	foreign key (team)	references team(id),
         unique  key username ( username ),
         primary key ( id )
 )  ENGINE=InnoDB;
 
-create table if not exists user_assigned_tasks (
-       user_id int not null references users(id),
-       task_id int not null references task(id),
-       primary key ( user_id, task_id )
+create table if not exists state (
+   id                             int not null auto_increment,
+   name				  varchar(200) not null,
+   description			  text,
+   next_state			  int,
+   next_state_requirement	  text,
+   foreign key (next_state) 	  references state(id),
+   primary key ( id )                
 )  ENGINE=InnoDB;
 
 create table if not exists project (
-  id                              int                             not null auto_increment,
-  state_id			  int not null references states(id),
+  id                              int not null auto_increment,
+  state_id			  int not null,
   name				  varchar(200) not null,
   description			  text,
-  start_date			  timestamp,
-  projected_end_date		  timestamp,
-  end_date			  timestamp,
-  created_by			  int references users(id),
-  created_date			  timestamp,
+  start_date			  datetime,
+  projected_end_date		  datetime,
+  end_date			  datetime,
+  created_by			  int,
+  created_date			  datetime,
   archived			  tinyint(0),
+  foreign key (state_id) references state(id),
+  foreign key (created_by) references users(id),
   primary key ( id )  
-)  ENGINE=InnoDB;
-
-create table if not exists project_sprints (
-       project_id int not null references project(id),
-       sprint_id int not null references sprint(id),
-       primary key (project_id, sprint_id)
 )  ENGINE=InnoDB;
 
 
 create table if not exists sprint (
-  id                              int                             not null auto_increment,
+  id                              int  not null auto_increment,
   name				  varchar(200) not null,
   description			  text,
-  team_id			  int references team(id),
-  start_date			  timestamp,
+  team_id			  int,
+  start_date			  datetime,
   sprint_length			  int not null default 14,
-  end_date			  timestamp,
-  created_by			  int references users(id),
-  created_date			  timestamp,
+  end_date			  datetime,
+  created_by			  int,
+  created_date			  datetime,
   archived			  tinyint(0),
+  foreign key (created_by) references users(id),
+  foreign key (team_id) references team(id),
   primary key ( id )  
 )  ENGINE=InnoDB;
 
+create table if not exists team_sprints (
+       team_id int not null references team(id),
+       sprint_id int not null references sprint(id),
+       primary key (team_id, sprint_id)
+) ENGINE=InnoDB;
+
+
+create table if not exists project_sprints (
+       project_id int not null,
+       sprint_id int not null,
+       foreign key (project_id) references project(id),
+       foreign key (sprint_id) references sprint(id),
+       primary key (project_id, sprint_id)
+)  ENGINE=InnoDB;
+
+
 create table if not exists sprint_unavailability (
   id                              int                             not null auto_increment,
-  user_id			  int references users(id),
-  start_date			  timestamp,
+  user_id			  int not null,
+  start_date			  datetime,
   start_half_day		  tinyint(1),
-  end_date			  timestamp,
+  end_date			  datetime,
   end_half_day		  	  tinyint(1),
+  foreign key (user_id) references users(id),
   primary key ( id )  
 )  ENGINE=InnoDB;
 
 create table if not exists story (
-  id                              int                             not null auto_increment,
-  sprint 			  int not null references sprint(id),
+  id                              int not null auto_increment,
+  sprint 			  int not null,
   priority 			  int not null default 100,
   estimate 			  int,
-  estimate_unit 		  int references estimate_unit(id),
+  estimate_unit 		  int,
+  remaining_work		  int,
+  completed_work		  int,
   name				  varchar(200) not null,
   summary			  text,  
   description			  text,
-  start_date			  timestamp,
-  end_date			  timestamp,
-  state_id			  int not null references state(id),
+  start_date			  datetime,
+  end_date			  datetime,
+  state_id			  int not null,
+  foreign key (sprint) references sprint(id),
+  foreign key (estimate_unit) references estimate_unit(id),
+  foreign key (state_id) references state(id),
   primary key ( id )  
 )  ENGINE=InnoDB;
 
 create table if not exists task (
-  id                              int                             not null auto_increment,
+  id                              int  not null auto_increment,
   estimate 			  int,
-  estimate_unit 		  int references estimate_unit(id),
-  story_id 			  int references story(id),
-  reviewed_by 			  int references users(id),
+  estimate_unit 		  int,
+  story_id 			  int,
+  reviewed_by 			  int,
   name				  varchar(200) not null,
   summary			  text,  
   description			  text,
-  start_date			  timestamp,
-  end_date			  timestamp,
-  state_id			  int not null references state(id),
+  start_date			  datetime,
+  end_date			  datetime,
+  state_id			  int not null,
+  foreign key (estimate_unit) 	  references estimate_unit(id),
+  foreign key (story_id)	  references story(id),
+  foreign key (reviewed_by) 	  references users(id),
+  foreign key (state_id)	  references state(id),
   primary key ( id )                
 )  ENGINE=InnoDB;
 
+
+
 create table if not exists task_dependancies ( 
-   task      	    	   int not null references task(id),
+   task      	    	   int not null,
    block_state 	    	   tinyint(1),
-   blocking_task    	   int not null references task(id),
+   blocking_task    	   int not null,
+   foreign key (task) 	   references task(id),
+   foreign key (blocking_task) references task(id),
    primary key(task, blocking_task)
 )  ENGINE=InnoDB;
 
+
 create table if not exists task_assigned_to (
-   id                              int                             not null auto_increment,
-   assigned_from_date 		   timestamp not null,
-   assigned_to_date 		   timestamp,
-   user_id 			   int not null references users(id),
-   task_id 			   int not null references task(id),
+   id                              int not null auto_increment,
+   assigned_from_date 		   datetime not null,
+   assigned_to_date 		   datetime,
+   current_task			   tinyint(1) not null default 0,
+   user_id 			   int not null,
+   task_id 			   int not null,
+   foreign key (user_id) 	   references users(id),
+   foreign key (task_id) 	   references task(id),
    key (user_id, task_id),
    primary key ( id )                
 )  ENGINE=InnoDB;
 
-
-create table if not exists state (
-   id                             int                             not null auto_increment,
-   name				  varchar(200) not null,
-   description			  text,
-   next_state			  int references state(id),
-   next_state_requirement	  text,
-   primary key ( id )                
+create table if not exists task_progress (
+   task_id 			   int not null,
+   remaining_work		   int not null,
+   progress_date		   datetime,
+   foreign key (task_id) 	   references task(id),
+   primary key (task_id, progress_date)                
 )  ENGINE=InnoDB;
 
-
 create table if not exists state_transitions (
-   id                             int                             not null auto_increment,
+   id                             int not null auto_increment,
    name				  varchar(200) not null,
    primary key ( id )                       
 ) ENGINE=InnoDB;
 
-create table if not exists estimate_unit (
-   id                              int                             not null auto_increment,
-   name				   varchar(20) not null,
-   abbreviation			   varchar(4),
-   primary key ( id )
-)  ENGINE=InnoDB;
-
 create table if not exists checklist (
-   id                              int                             not null auto_increment,
+   id                              int not null auto_increment,
    name				   varchar(200) not null,
-   task				   int references task(id), 
+   task				   int,
+   foreign key (task)  		   references task(id), 
    primary key ( id )
 )  ENGINE=InnoDB;
 
 create table if not exists checklist_items (
-   id                              int                             not null auto_increment,
+   id                              int not null auto_increment,
    name				   varchar(200) not null,
-   checklist			   int references checklist(id),
+   checklist			   int,
+   foreign key (checklist) 	   references checklist(id),
    primary key ( id )
 )  ENGINE=InnoDB;
+
