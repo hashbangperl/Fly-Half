8003123df567327b4127e2d172786b205169c5db
Aaron Trevena <aaron.trevena@gmail.com>
2012-01-09 20:55:43 +0000

    added

---
+++ lib/FlyHalf/Controller/Team.pm
@@ -0,0 +1,49 @@
+package FlyHalf::Controller::Team;
+use Moose;
+use namespace::autoclean;
+
+BEGIN {extends 'Catalyst::Controller'; }
+
+=head1 NAME
+
+FlyHalf::Controller::User - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+=head2 view
+
+=cut
+
+sub view : Local : Args( 1 ) {
+    my ($self, $c, $team_id) = @_;
+    $c->stash->{template} = 'team/view.tt';
+
+    $c->stash->{this_team} = $c->model( 'DBIC::Team' )->search(
+	{'me.id' => $team_id},
+	{ prefetch => { 'team_sprints' => 'sprints' } }
+	)->first;
+
+    return 1;
+
+}
+
+=head1 AUTHOR
+
+Aaron Trevena
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
