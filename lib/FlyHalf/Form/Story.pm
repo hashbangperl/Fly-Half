d6588988e32642fe745ce83c006ce0963cf6c7bd
Aaron Trevena <aaron.trevena@gmail.com>
2012-06-26 22:46:37 +0100

    updated to use new base form class and be DRY

--- lib/FlyHalf/Form/Story.pm
+++ lib/FlyHalf/Form/Story.pm
@@ -5,8 +5,7 @@ use strict;
 use warnings;
 use HTML::FormHandler::Moose;
 
-extends 'HTML::FormHandler::Model::DBIC';
-with 'HTML::FormHandler::Render::Simple';
+extends 'FlyHalf::Form::Base';
 
 has_field 'name'        => ( type => 'Text',     required => 1 );
 has_field 'ref_code'    => ( type => 'Text',     required => 1 );
@@ -17,23 +16,5 @@ has_field 'estimate_unit' => ( type=> 'Select' );
 has_field 'estimate'    => ( type => 'Text', required => 0 );
 has_field 'sprint'      => ( type => 'Select');
 has_field 'priority'    => ( type => 'Text', required => 0 );
-
-sub options_estimate_unit {
-   my $self = shift;
-   return unless $self->schema;
-   my @rows =
-      $self->schema->resultset( 'EstimateUnit' )->
-         search( {}, { order_by => ['name'] } )->all;
-   return [ map { $_->id, $_->to_string } @rows ];
-}
-
-sub options_sprint {
-   my $self = shift;
-   return unless $self->schema;
-   my @rows =
-      $self->schema->resultset( 'Sprint' )->
-         search( {}, { order_by => ['name'] } )->all;
-   return [ map { $_->id, $_->name } @rows ];
-}
 
 1;
