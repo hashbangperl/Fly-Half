47793cd1b58ebaa67ae41b187bdcd62356837038
Aaron Trevena <aaron.trevena@gmail.com>
2012-07-15 11:58:28 +0100

    Integrated date picker, tidied up form handling (gave up on compound fields for now)

--- lib/FlyHalf/Form/Story.pm
+++ lib/FlyHalf/Form/Story.pm
@@ -5,16 +5,23 @@ use strict;
 use warnings;
 use HTML::FormHandler::Moose;
 
-extends 'FlyHalf::Form::Base';
+extends 'HTML::FormHandler::Model::DBIC';
+with 'HTML::FormHandler::Render::Simple';
+with 'FlyHalf::Form::Lookup';
 
 has_field 'name'        => ( type => 'Text',     required => 1 );
 has_field 'ref_code'    => ( type => 'Text',     required => 1 );
 has_field 'summary'     => ( type => 'TextArea', required => 0 );
 has_field 'description' => ( type => 'TextArea', required => 0 );
 has_field 'start_date'  => ( type => 'Date', required => 0);
-has_field 'estimate_unit' => ( type=> 'Select' );
-has_field 'estimate'    => ( type => 'Text', required => 0 );
+
 has_field 'sprint'      => ( type => 'Select');
+
 has_field 'priority'    => ( type => 'Text', required => 0 );
+
+has_field 'estimate'    => ( type => 'Text', required => 0 );
+has_field 'estimate_unit' => ( type=> 'Select' );
+
+
 
 1;
