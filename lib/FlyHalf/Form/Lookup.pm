445fa0263ee416b04ba6a86aad0a2b7e8670b63f
Aaron Trevena <aaron.trevena@gmail.com>
2012-08-20 06:28:58 +0100

    reworked forms and form validation yet again, now with jquery validation in forms before submit

---
+++ lib/FlyHalf/Form/Lookup.pm
@@ -0,0 +1,25 @@
+package FlyHalf::Form::Lookup;
+
+use Moose::Role;
+
+sub options_estimate_unit {
+   my $self = shift;
+   return unless $self->schema;
+   my @rows =
+      $self->schema->resultset( 'EstimateUnit' )->
+         search( {}, { order_by => ['name'] } )->all;
+   return [ map { $_->id, $_->to_string } @rows ];
+}
+
+
+sub options_sprint {
+   my $self = shift;
+   return unless $self->schema;
+   my @rows =
+      $self->schema->resultset( 'Sprint' )->
+         search( {}, { order_by => ['name'] } )->all;
+   return [ map { $_->id, $_->name } @rows ];
+}
+
+
+1;
---
+++ root/static/javascript/form_validation.js
@@ -0,0 +1,4 @@
+var validationJSON;
+$(document).ready(function(){
+        validationJSON = JSON.parse(unescape($("#validation_json").val() ) );
+}); // end document.ready
